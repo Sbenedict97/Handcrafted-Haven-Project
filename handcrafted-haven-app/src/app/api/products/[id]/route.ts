1a5d5689b3e9542165b540e543bc8c9d9bda0fa4
lechavezlbyu <lchavez1@byupathway.edu>
2025-02-21 16:08:02 -0400

    Fixed a deploy error in the api/products/[id]/route.ts

--- handcrafted-haven-app/src/app/api/products/[id]/route.ts
+++ handcrafted-haven-app/src/app/api/products/[id]/route.ts
@@ -2,21 +2,19 @@ import { NextResponse } from "next/server";
 import { getProductById } from "@/app/lib/databaseActions";
 
 
-export const GET = async (request: Request, { params }: { params: { id: string } }) => {
+export const GET = async (request: Request, context: { params: { id: string } }) => {
   try {
-    const { id } = await params;
-
+    const { id } = context.params;
+    console.log("ID we get from the API:", id);
 
     const product = await getProductById(id);
-
-
     if (!product) {
-      return NextResponse.json({ message: "Product not found" }, { status: 404 });
+      return NextResponse.json({ message: 'Product not found' }, { status: 404 });
     }
 
     return NextResponse.json(product);
   } catch (error) {
-    console.error("Error fetching product by ID:", error);
-    return NextResponse.json({ error: "Failed to fetch product" }, { status: 500 });
+    console.error("Error fetching product:", error);
+    return NextResponse.json({ message: 'Server Error' }, { status: 500 });
   }
 };
